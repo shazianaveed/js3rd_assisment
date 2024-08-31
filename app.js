175f3784e22879160647d6600845bf5a46f37d2c
shazianaveed <160173580+shazianaveed@users.noreply.github.com>
2024-09-01 02:23:02 +0500

    added js and html

---
+++ app.js
@@ -0,0 +1,33 @@
+       // Q#1
+    // var a =10;
+    // alert("The value of a is:" + a);
+
+// var a = 10;
+// alert("The value of ++a is: " + ++a  +" Now the value of a is: 11 ");
+
+// var a = 11;
+// alert("The value of a++ is: " + a++ + " Now the value of a is : " + a++);
+
+// var a = 12;
+// alert("The value of --b is: " + --b + " Now the value of a is: "+ b--);
+
+//Q#2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+//             //  1 - 0 +  1 + 1 = 3
+// alert("a is: ? 2  "<br> " b is 1  "<br>" result is " + result);
+
+//Q#3
+
+// let greetUser = "Assalam.o.alikum Welcome to my page";
+// let userName = "please Enter your name.";
+// alert( greetUser + userName);
+
+
+//Q#4
+
+
+    
+
+
+
